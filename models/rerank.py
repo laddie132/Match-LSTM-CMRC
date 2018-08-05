da7ad8ff161aadb9b5338bbd49e540f6c9edd32c
Han <liuhan132@foxmail.com>
2018-08-05 12:52:47 +0800

    Add m-reader rerank

--- models/rerank.py
+++ models/rerank.py
@@ -28,35 +28,37 @@ class ReRanker(torch.nn.Module):
         super(ReRanker, self).__init__()
 
         word_embedding_size = 300
-        hidden_size = 150
-        hidden_mode = 'GRU'
-        emb_dropout_p = 0.1
-        dropout_p = 0.4
+        hidden_size = 100
+        hidden_mode = 'LSTM'
+        emb_dropout_p = 0.2
+        dropout_p = 0.2
 
-        encoder_bidirection = True
-        encoder_direction_num = 2 if encoder_bidirection else 1
-
-        measure_bidirection = True
-        measure_direction_num = 2 if measure_bidirection else 1
+        self.num_align_hops = 2
 
         self.embedding = GloveEmbedding(dataset_h5_path=dataset_h5_path)
         self.encoder = MyRNNBase(mode=hidden_mode,
                                  input_size=word_embedding_size,
                                  hidden_size=hidden_size,
-                                 bidirectional=encoder_bidirection,
+                                 bidirectional=True,
                                  dropout_p=emb_dropout_p)
-        encoder_out_size = hidden_size * encoder_direction_num
 
-        self.seq_to_seq_att = SeqToSeqAtten()
-        self.linear_fusion = LinearFusion(encoder_out_size,
-                                          dropout_p=dropout_p)
-        self.measure_rnn = MyRNNBase(mode=hidden_mode,
-                                     input_size=encoder_out_size,
-                                     hidden_size=hidden_size,
-                                     bidirectional=measure_bidirection,
-                                     dropout_p=dropout_p)
-        measure_out_size = hidden_size * measure_direction_num
-        self.linear_log_softmax = LinearLogSoftmax(measure_out_size)
+        self.aligner = torch.nn.ModuleList([SeqToSeqAtten() for _ in range(self.num_align_hops)])
+        self.aligner_sfu = torch.nn.ModuleList([SFU(input_size=hidden_size * 2,
+                                                    fusions_size=hidden_size * 2 * 3) for _ in
+                                                range(self.num_align_hops)])
+        self.ans_aligner = torch.nn.ModuleList([SeqToSeqAtten() for _ in range(self.num_align_hops)])
+        self.ans_aligner_sfu = torch.nn.ModuleList([SFU(input_size=hidden_size * 2,
+                                                         fusions_size=hidden_size * 2 * 3)
+                                                     for _ in range(self.num_align_hops)])
+        self.aggregation = torch.nn.ModuleList([MyRNNBase(mode=hidden_mode,
+                                                          input_size=hidden_size * 2,
+                                                          hidden_size=hidden_size,
+                                                          bidirectional=True,
+                                                          dropout_p=dropout_p,
+                                                          enable_layer_norm=False)
+                                                for _ in range(self.num_align_hops)])
+
+        self.linear_log_softmax = LinearLogSoftmax(hidden_size*2)
 
     def forward(self, context, question, candidate_answer):
         # word-level embedding: (seq_len, batch, embedding_size)
@@ -73,44 +75,45 @@ class ReRanker(torch.nn.Module):
         cand_s = []
         for i in range(k):
 
-            qus_ans = []
-            qus_ans_len = []
-            cand_ans = candidate_answer[:, i, :]    # (batch, ans_len)
+            ans_encode_list = []
+            ans_len = []
+            cand_ans = candidate_answer[:, i, :]    # (batch, 2)
             for j in range(batch):
                 cur_ans = cand_ans[j, :]
                 cur_ans_encode = context_encode[cur_ans[0]:(cur_ans[1]+1), j, :]
+                ans_encode_list.append(cur_ans_encode)
+                ans_len.append(cur_ans_encode.shape[0])
 
-                # question de-pad
-                cur_qus_length = question_length[j]
-                cur_qus_encode = question_encode[:cur_qus_length, j, :]
-
-                cur_qus_ans = torch.cat([cur_ans_encode, cur_qus_encode], dim=0)   # (ans_len+qus_len, hidden_size)
-                qus_ans.append(cur_qus_ans)
-                qus_ans_len.append(cur_qus_ans.shape[0])
-
-            # padding to same qus_ans length
-            max_qus_ans_len = np.max(qus_ans_len)
-            qus_ans_mask = []
+            # padding to answer encode
+            max_ans_len = np.max(ans_len)
+            hidden_size = context_encode.shape[-1]
+            ans_encode = context_encode.new_zeros((max_ans_len, batch, hidden_size))
+            ans_mask = context_encode.new_zeros((batch, max_ans_len))
             for j in range(batch):
-                cur_len, hidden_size = qus_ans[j].shape[0], qus_ans[j].shape[1]
-                zeros_len = max_qus_ans_len - cur_len
-                new_zeros = qus_ans[j].new_zeros((zeros_len, hidden_size))
-                qus_ans[j] = torch.cat([qus_ans[j], new_zeros], dim=0)
+                cur_len = ans_encode_list[j].shape[0]
+                ans_encode[:cur_len, j, :] = ans_encode_list[j]
+                ans_mask[j, :cur_len] = 1
 
-                cur_mask = torch.cat([qus_ans[j].new_ones((cur_len,)), qus_ans[j].new_zeros((zeros_len,))], dim=0)
-                qus_ans_mask.append(cur_mask)
-            qus_ans = torch.stack(qus_ans, dim=1)   # (qus_ans_len, batch, hidden_size)
-            qus_ans_mask = torch.stack(qus_ans_mask, dim=0)   # (batch, qus_ans_len)
+            align_ans = ans_encode
+            align_qt = question_encode
+            for i in range(self.num_align_hops):
+                # question-context align: (seq_len, batch, hidden_size*2)
+                ct_align_qt, _ = self.aligner[i](question_encode, context_encode, context_mask)
+                align_qt = self.aligner_sfu[i](align_qt, torch.cat([ct_align_qt,
+                                                                    align_qt * ct_align_qt,
+                                                                    align_qt - ct_align_qt], dim=-1))
 
-            # (qus_ans_len, batch, hidden_size)
-            qus_ans_aware, alpha = self.seq_to_seq_att(qus_ans, context_encode, context_mask)
-            m = self.linear_fusion(qus_ans, qus_ans_aware)
+                # answer-question align: (seq_len, batch, hidden_size*2)
+                qt_align_ans, _ = self.ans_aligner[i](align_ans, align_qt, question_mask)
+                hat_ans = self.ans_aligner_sfu[i](align_ans, torch.cat([qt_align_ans,
+                                                                        align_ans * qt_align_ans,
+                                                                        align_ans - qt_align_ans], dim=-1))
 
-            # (qus_ans_len, batch, hidden_size)
-            ans_measure, _ = self.measure_rnn(m, qus_ans_mask)
-            ans_s, _ = torch.max(ans_measure, dim=0)    # (batch, hidden_size)
-            cand_s.append(ans_s)
-        cand_s = torch.stack(cand_s, dim=0)  # (k, batch, hidden_size)
+                # aggregation: (seq_len, batch, hidden_size*2)
+                align_ans, align_ans_last = self.aggregation[i](hat_ans, ans_mask)
+
+            cand_s.append(align_ans_last)
+        cand_s = torch.stack(cand_s, dim=0)  # (k, batch, hidden_size*2)
         cand_score = self.linear_log_softmax(cand_s)    # (batch, k)
 
         _, max_cand_k = torch.max(cand_score, dim=-1)   # (batch,)
